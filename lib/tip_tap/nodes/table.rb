08cfb134dac676414f65ba7c08ded1381b89b7e7
Kenton Glass <git@kenton.glass>
2025-09-03 21:39:56 -0400

    Add appearance data attribute to Table

--- lib/tip_tap/nodes/table.rb
+++ lib/tip_tap/nodes/table.rb
@@ -8,6 +8,9 @@ module TipTap
       self.type_name = "table"
       self.html_tag = :table
       self.html_class_name = "table"
+      self.html_data_attributes = {
+        table_appearance_value: "bordered",
+      }
 
       def table_row(&block)
         raise ArgumentError, "Block required" if block.nil?
