8d9285a9d4203cdc2bccf01009d42fcf88969e9a
Kenton Glass <git@kenton.glass>
2025-10-14 21:26:42 -0400

    Add link mark support to Image nodes

--- lib/tip_tap/nodes/image.rb
+++ lib/tip_tap/nodes/image.rb
@@ -7,12 +7,40 @@ module TipTap
     class Image < Node
       self.type_name = "image"
 
+      attr_reader :marks
+
+      def initialize(attrs = {}, marks: [])
+        super
+        @attrs = attrs.deep_stringify_keys
+        @marks = Array(marks).map(&:deep_stringify_keys)
+      end
+
       def include_empty_content_in_json?
         false
       end
 
       def to_html
-        image_tag(src, alt: alt)
+        image = image_tag(src, alt: alt)
+
+        if marks.any? { |mark| mark["type"] == 'link' }
+          content_tag(:a, image, href: link_href, target: link_target)
+        else
+          image
+        end
+      end
+
+      def self.from_json(json)
+        json.deep_stringify_keys!
+        new(
+          json["attrs"] || {},
+          marks: json["marks"] || []
+        )
+      end
+
+      def to_h
+        data = { type: type_name, attrs: attrs }
+        data[:marks] = marks unless marks.empty?
+        data
       end
 
       def to_markdown(context = Markdown::Context.root)
@@ -30,6 +58,14 @@ module TipTap
 
       def src
         attrs["src"]
+      end
+
+      def link_href
+        marks.find { |mark| mark["type"] == "link" }&.dig("attrs", "href")
+      end
+
+      def link_target
+        marks.find { |mark| mark["type"] == "link" }&.dig("attrs", "target")
       end
     end
   end
